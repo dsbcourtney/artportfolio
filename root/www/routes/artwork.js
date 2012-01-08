6ac5f5dc39313197800219ac58ef5cac476beae8
errumm <lewis.barclay@googlemail.com>
2012-01-08 12:49:03 +0000

    ammended views to point to the right routes

--- root/www/routes/artwork.js
+++ root/www/routes/artwork.js
@@ -1,5 +1,5 @@
 
-module.exports = function(){
+module.exports = function(app){
   
   app.get('/artwork/:artistName/:collectionName/:artworkTitle', function(req, res){
      res.render('artwork.jade', {title : 'Art Rebellion: [Artwork]', pageTitle: '[Artwork]'});
