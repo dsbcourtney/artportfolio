c260e1f58f29bb5ff2dc68ab6483c9eafb09c917
errumm <lewis.barclay@googlemail.com>
2012-01-09 08:45:04 +0000

    continued mongoose integration - working mainly on figuring out admin side of things. Added beginning of admin jade templates for the admin site.

--- root/www/routes/artist.js
+++ root/www/routes/artist.js
@@ -1,44 +1,60 @@
+module.exports = function(app, mongoose) {
 
-module.exports = function(app, mongoose){
-
-  app.get('/artist/:artistName', function(req, res){
-   res.render('artist.jade', {title : 'Art Rebellion: [Artist Name]', pageTitle: '[Artist Name]'});
-  });
-
-  //read
-  app.get('/admin/artist/', function(req, res){
-   res.render('artist.jade', {title : 'Art Rebellion: [Artist Name]', pageTitle: '[Artist Name]'});
-  });
-  
-  //create
-  app.post('/admin/artist/:artistName', function(req, res){
-   res.render('artist.jade', {title : 'Art Rebellion: [Artist Name]', pageTitle: '[Artist Name]'});
-  });
-  
-  //update
-  app.put('/admin/artist/:artistName', function(req, res){
-   res.render('artist.jade', {title : 'Art Rebellion: [Artist Name]', pageTitle: '[Artist Name]'});
+  app.get('/artists/:artistName', function(req, res) {
+    res.render('artist.jade', {title : 'Art Rebellion: [Artist Name]', pageTitle: '[Artist Name]'});
   });
 
-  //delete
-  app.del('/admin/artist/:artistName', function(req, res){
-   res.render('artist.jade', {title : 'Art Rebellion: [Artist Name]', pageTitle: '[Artist Name]'});
+  //read all artists
+  app.get('/admin/artists', function(req, res) {
+    //
+
+    var Artist = mongoose.model('Artist');
+
+    Artist.find({}, function(err, artists) {
+      res.render('admin/list-artists.jade',
+              {title : 'Art Rebellion Admin: List Artists', pageTitle: 'List Artists', artists:artists});
+    });
+
   });
-  
+
+  //create single artist form
+  app.get('/admin/artists/new', function(req, res) {
+    res.render('artist.jade', {title : 'Art Rebellion: [Artist Name]', pageTitle: '[Artist Name]'});
+  });
+
+  //read single artist
+  app.get('/admin/artists/:artistId', function(req, res) {
+    res.render('artist.jade', {title : 'Art Rebellion: [Artist Name]', pageTitle: '[Artist Name]'});
+  });
+
+  //create artist
+  app.post('/admin/artists/', function(req, res) {
+    res.render('artist.jade', {title : 'Art Rebellion: [Artist Name]', pageTitle: '[Artist Name]'});
+  });
+
+  //update single artist
+  app.put('/admin/artists/:artistId', function(req, res) {
+    res.render('artist.jade', {title : 'Art Rebellion: [Artist Name]', pageTitle: '[Artist Name]'});
+  });
+
+  //update single artist
+  app.del('/admin/artists/:artistId', function(req, res) {
+    res.render('artist.jade', {title : 'Art Rebellion: [Artist Name]', pageTitle: '[Artist Name]'});
+  });
 };
 
 /*
-var Artist = mongoose.model('Artist');
+ var Artist = mongoose.model('Artist');
 
-var tempArtist = new Artist({
-  slug        : "temp-artist1",
-  name        : "Artist Name",
-  biography   : "biography",
-  dateAdded   : new Date(),
-  dateUpdated : new Date(),
-  websiteUrl  : "http://www.domain.com"
-});
+ var tempArtist = new Artist({
+ slug        : "temp-artist1",
+ name        : "Artist Name",
+ biography   : "biography",
+ dateAdded   : new Date(),
+ dateUpdated : new Date(),
+ websiteUrl  : "http://www.domain.com"
+ });
 
-tempArtist.save();
+ tempArtist.save();
 
-*/
+ */
