3d81cdf9a657a659f78aa334539d42c54bc8ecda
David Courtney <dsbcourtney@gmail.com>
2012-01-17 20:38:35 +0000

    Remove all existing of dave_branch

--- root/www/routes/default.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = function(app, mongoose){
-  
-  app.get('/', function(req, res){
-     res.render('index.jade', {title : 'Art Rebellion', pageTitle: 'The Gallery'});
-  });  
-  
-};
