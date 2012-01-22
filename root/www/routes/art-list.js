cdb07e8092d1631559fde62c8d8359a18888b824
errumm <lewis.barclay@googlemail.com>
2012-01-22 11:29:12 +0000

    fixed conflicts

--- root/www/routes/art-list.js
+++ root/www/routes/art-list.js
@@ -4,14 +4,14 @@ module.exports = function(app, mongoose, vdp) {
   app.get('/art-list', function(req, res) {
     var locals = {title : 'Art Rebellion', pageTitle: '[All Art]'};
 
-    vdp.getPublicViewData(thenRender, 'tagged-artwork.jade', locals, res);
+    vdp.getPublicViewData(thenRender, 'tagged-artwork.jade', locals);
   });
 
 
   app.get('/art-list/:tag', function(req, res) {
     var locals = {title : 'Art Rebellion', pageTitle: '[Tag]'};
 
-    vdp.getPublicViewData(thenRender, 'tagged-artwork.jade', locals, res);
+    vdp.getPublicViewData(thenRender, 'tagged-artwork.jade', locals);
 
   });
 };
